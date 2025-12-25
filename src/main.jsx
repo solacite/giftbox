3f5057c6b7cc78c215dcd88e7cdced39ec20dd82
solacite <julia.nguyen.do@gmail.com>
2025-12-25 16:48:12 -0500

    finally got initial setup to bald html/css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+function MyButton() {
+  return (
+    <button>bawefhwoef</button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>welc to the gang</h1>
+      <MyButton />
+    </div>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MyApp />
+  </StrictMode>
+);
