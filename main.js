bfe26bd7a0497d1dbd2bfb1e4929d09c5609c10a
solacite <julia.nguyen.do@gmail.com>
2025-12-25 16:42:07 -0500

    initialize react app probably? i might have messed up

---
+++ main.js
@@ -0,0 +1,14 @@
+function MyButton() {
+    return (
+        <button>I'm a button</button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>welc to the gang</h1>
+            <MyButton />
+        </div>
+    );
+}
